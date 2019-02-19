70c3d6edea665c70743f900bb29ee515bc3e12af
Your Name <you@example.com>
2019-02-19 20:13:05 +0800

    初始2.0

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './router'
-
 import store from './store'
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+
+Vue.use(MuseUI)
+
 new Vue({
   el: '#app',
-
   router,
   render: h => h(App),
   store
-
 })
